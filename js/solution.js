d64043482e83ff00983aa227823eb9040fffc368
Alexey Baranov <a.baranov@1win.pro>
2024-07-20 01:56:11 +0300

    fix: 'solution' by using destructuring assignment

--- js/solution.js
+++ js/solution.js
@@ -6,9 +6,7 @@ function solution(nums){
     for (let i = 0; i < nums.length; i++) {
         for (let j = 0; j < nums.length - 1; j++) {
             if (nums[j] > nums[j + 1]) {
-                const temp = nums[j];
-                nums[j] = nums[j + 1];
-                nums[j + 1] = temp;
+                ([nums[j], nums[j + 1]] = [nums[j + 1], nums[j]]);
             }
         }
     }
