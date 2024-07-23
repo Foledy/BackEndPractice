b6bf7a99123bc938de90e78d0b5125c85acdbcca
Alexey Baranov <a.baranov@1win.pro>
2024-07-23 21:51:44 +0300

    upd: using cache and solved problem with cycle objects

--- js/getStringCount.js
+++ js/getStringCount.js
@@ -1,25 +1,50 @@
-function getStringCount(data) {
-    if (data == null) {
+const cache = new WeakMap();
+
+function extern_getStringCount(data) {
+    return getStringCount(data);
+}
+
+function getStringCount(data, alreadyUsed = []) {
+    if (data === null || alreadyUsed.includes(data)) {
         return 0;
+    }
+
+    alreadyUsed.push(data);
+
+    if (cache.has(data)) {
+        return cache[data];
     }
 
     const type = typeof data;
 
     if (type !== "object") {
-        return type === "string" ? 1 : 0;
+        cache[data] = type === "string" ? 1 : 0;
+        return cache[data];
     }
 
     let counter = 0;
 
     if (Array.isArray(data)) {
         for (const item of data) {
-            counter += getStringCount(item);
+            counter += getStringCount(item, alreadyUsed);
         }
     } else {
         for (const item of Object.keys(data)) {
-            counter += getStringCount(data[item]);
+            counter += getStringCount(data[item], alreadyUsed);
         }
     }
 
-    return counter;
+    cache[data] = counter;
+    return cache[data];
 }
+
+const obj = {
+    name: 'obj'
+}
+
+const cycleObj = {
+    myBrother: obj,
+    myAge: 15
+}
+
+obj.myBrother = cycleObj;
